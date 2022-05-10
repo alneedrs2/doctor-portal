c34cef3462561c78ddd762c773cfc064154eedea
Alneed Roy <alneedrs2@gmail.com>
2022-05-10 20:41:06 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Shared/Header/Header';
+import { Routes, Route} from "react-router-dom";
+import Home from './components/Pages/Home/Home';
+import About from './components/Pages/About/About';
+import Appoinment from './components/Pages/Appoinment/Appoinment';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="appoinment" element={<Appoinment />} />
+      </Routes>
     </div>
   );
 }
