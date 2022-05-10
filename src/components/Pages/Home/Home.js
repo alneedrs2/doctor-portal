9c6425bb6aabff2bb0664bc8175722236daec86d
Alneed Roy <alneedrs2@gmail.com>
2022-05-11 03:00:06 +0600

    footer and warning issue solved

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./Banner/Banner";
 import Contact from "./Contact/Contact";
 import Exceptional from "./Exceptional/Exceptional";
+import Footer from "./Footer/Footer";
 import Info from "./Info/Info";
 import MakeAppoinment from "./MakeAppoinment/MakeAppoinment";
 import Services from "./Services/Services";
@@ -17,6 +18,7 @@ const Home = () => {
       <MakeAppoinment></MakeAppoinment>
       <Testimonials></Testimonials>
       <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 };
