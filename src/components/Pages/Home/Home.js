9c1c9dc6fbbbae93f470db5246cf9379f45e1767
Alneed Roy <alneedrs2@gmail.com>
2022-05-11 02:46:27 +0600

    contact us added

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "./Banner/Banner";
+import Contact from "./Contact/Contact";
 import Exceptional from "./Exceptional/Exceptional";
 import Info from "./Info/Info";
 import MakeAppoinment from "./MakeAppoinment/MakeAppoinment";
@@ -15,6 +16,7 @@ const Home = () => {
       <Exceptional></Exceptional>
       <MakeAppoinment></MakeAppoinment>
       <Testimonials></Testimonials>
+      <Contact></Contact>
     </div>
   );
 };
